3266fb68584f2aa2b5e60b8367960c2b144dcacb
Kris Heinrich <krisheinrich@gmail.com>
2017-04-17 13:54:55 -0700

    setup react-router, make App and SongList components, establish GraphQL connection with ApolloProvider

--- client/index.js
+++ client/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+import App from './components/App';
+import SongList from './components/SongList';
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      {/* <SongList /> */}
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(
